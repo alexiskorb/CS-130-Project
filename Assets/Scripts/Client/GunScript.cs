66f976f2270f38b4d8f6bc7268a79b2c26c6dae6
melodiebutz <melodie.butz@gmail.com>
2018-04-27 14:56:09 -0700

    Can print out player's Steamworks username to console

--- Assets/Scripts/Client/GunScript.cs
+++ Assets/Scripts/Client/GunScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Steamworks;
 
 public class GunScript : MonoBehaviour {
 
@@ -9,7 +10,10 @@ public class GunScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+		if(SteamManager.Initialized) {
+			string name = SteamFriends.GetPersonaName();
+			Debug.Log(name);
+		}
 	}
 	
 	// Update is called once per frame
