27457c63e6c11dd1fb0128c945c8bb70c269af71
Alexis Korb <alexiskorb@gmail.com>
2018-05-05 22:57:14 -0700

    Added UI Features
    -Created a scrollable match viewer for joining matches
    -Let players join a match lobby
    -Refactored UI
    -Reset player and match names when leaving a matchmaking lobby

---
+++ Assets/Scripts/Client/UI/CreateMatchUI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateMatchUI : MonoBehaviour {
+
+    public GameObject playerNameInput;
+    public GameObject matchNameInput;
+    public GameObject createMatchError;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        createMatchError.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SubmitMainPlayerName()
+    {
+        string name = playerNameInput.GetComponent<InputField>().text;
+        GameManager.Instance.MainPlayerName = name;
+    }
+
+    public void SubmitMatchName()
+    {
+        string name = matchNameInput.GetComponent<InputField>().text;
+        GameManager.Instance.MatchName = name;
+    }
+
+    public bool CreateMatch()
+    {
+        if (GameManager.Instance.MatchReady)
+        {
+            GameManager.Instance.CreateMatch();
+            return true;
+        }
+        else
+        {
+            createMatchError.SetActive(true);
+            return false;
+        }
+    }
+
+    public void ResetMenu()
+    {
+        playerNameInput.GetComponent<InputField>().text = "";
+        matchNameInput.GetComponent<InputField>().text = "";
+        createMatchError.SetActive(false);
+    }
+}
