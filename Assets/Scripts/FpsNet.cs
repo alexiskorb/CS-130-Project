51af11aecc4a331ca253ef4a2817df43f9b25d28
Joseph Garvey <joegarvey23@gmail.com>
2018-05-08 21:25:18 -0700

    Added a demo scene showing a server handling multiple clients.

--- Assets/Scripts/FpsNet.cs
+++ Assets/Scripts/FpsNet.cs
@@ -3,111 +3,337 @@ using System;
 using System.Runtime.InteropServices;
 
 namespace FpsNetcode {
-
 	public static class Netcode {
+		// @TODO This part of Netcode is heavily game-specific, so we'll probably want to move
+		// this code to the Game class. 
 
-		public enum PacketType {
+		// @func ApplySnapshot
+		// @desc Applies the given snapshot to the game object. 
+		public static void ApplySnapshot(ref GameObject gameObject, PlayerSnapshot snapshot)
+		{
+			gameObject.transform.position = snapshot.m_position;
+		}
+
+		public enum PacketType : int {
 			CONNECT,
 			CLIENT_SNAPSHOT,
 			CLIENT_CMD,
+			DISCONNECT
 		}
 
+		// @doc Consider naming commands after actions and not key presses
+		// to leave open the possibility for client's having alternate key bindings. 
 		public enum CmdType {
-			W, A, S, D,
-			LEFT_CLICK
+			FORWARD, BACKWARD, LEFT, RIGHT,
+			PRIMARY_WEAPON, 
 		}
 
-		public struct PacketHeader {
-			public PacketType type;
-			public uint seqno;
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public class CmdPacket {
+			public PacketHeader m_header;	
+			public CmdType m_cmd;
+
+			public CmdPacket(uint seqno, CmdType cmd)
+			{
+				m_header = new PacketHeader(PacketType.CLIENT_CMD, seqno);
+				m_cmd = cmd;
+			}
+
+			private CmdPacket(PacketHeader header, CmdType cmd)
+			{
+				m_header = header;
+				m_cmd = cmd;
+			}
+
+			public static byte[] Serialize(CmdPacket cmdPacket)
+			{
+				byte[] buf = Malloc(cmdPacket);
+				PacketHeader.Serialize(cmdPacket.m_header, ref buf);
+				MemCpy((int)cmdPacket.m_cmd, buf, Marshal.SizeOf(cmdPacket.m_header));
+				return buf;
+			}
+
+			public static CmdPacket Deserialize(byte[] buf)
+			{
+				PacketHeader header = PacketHeader.Deserialize(buf);
+				CmdType cmd = (CmdType)BitConverter.ToInt32(buf, Marshal.SizeOf(header));
+				return new CmdPacket(header, cmd);
+			}
 		}
 
-		public struct CmdPacket {
-			public PacketHeader header;
-			public CmdType cmd;
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public class PlayerSnapshot {
+			public PacketHeader m_header;
+			public int m_serverId;
+			public Vector3 m_position;
+
+			private PlayerSnapshot(PacketHeader header, int serverId, Vector3 position)
+			{
+				m_header = header;
+				m_serverId = serverId;
+				m_position = position;
+			}
+
+			public PlayerSnapshot(uint seqno, int serverId, Vector3 position)
+			{
+				m_header = new PacketHeader(PacketType.CLIENT_SNAPSHOT, seqno);
+				m_serverId = serverId;
+				m_position = position;
+			}
+
+			public static byte[] Serialize(PlayerSnapshot snapshot)
+			{
+				byte[] buf = Malloc(snapshot);
+				PacketHeader.Serialize(snapshot.m_header, ref buf);
+				MemCpy(snapshot.m_serverId, buf, Marshal.SizeOf(snapshot.m_header));
+				MemCpy(snapshot.m_position, buf, Marshal.SizeOf(snapshot.m_header) + sizeof(int));
+				return buf;
+			}
+
+			public static PlayerSnapshot Deserialize(byte[] buf)
+			{
+				PacketHeader header = PacketHeader.Deserialize(buf);
+				Vector3 position = new Vector3();
+				int serverId = BitConverter.ToInt32(buf, Marshal.SizeOf(header));
+				DeserializeVec3(ref position, buf, Marshal.SizeOf(header) + sizeof(int));
+				return new PlayerSnapshot(header, serverId, position);
+			}
 		}
 
-		//[DllImport("msvcrt.dll", SetLastError = false)]
-		//static extern void memcpy(void* dest, void* src, uint num);
+		// @doc Everything after this is game-independent and shouldn't really be touched. 
 
-		public static PacketHeader GetHeader(byte[] buf)
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public class ConnectPacket {
+			public PacketHeader m_header;
+			public int m_serverId;
+
+			public ConnectPacket(uint seqno, int serverId)
+			{
+				m_header = new PacketHeader(PacketType.CONNECT, seqno);
+				m_serverId = serverId;
+			}
+
+			private ConnectPacket(PacketHeader header, int serverId)
+			{
+				m_header = header;
+				m_serverId = serverId;
+			}
+
+			public static byte[] Serialize(ConnectPacket connect)
+			{
+				byte[] buf = Malloc(connect);
+				PacketHeader.Serialize(connect.m_header, ref buf);
+				MemCpy(connect.m_serverId, buf, Marshal.SizeOf(connect.m_header));
+				return buf;
+			}
+
+			public static ConnectPacket Deserialize(byte[] buf)
+			{
+				PacketHeader header = PacketHeader.Deserialize(buf);
+				int serverId = BitConverter.ToInt32(buf, Marshal.SizeOf(header));
+				return new ConnectPacket(header, serverId);
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public class PacketHeader {
+			public PacketType m_type;
+			public uint m_seqno;
+
+			public PacketHeader(PacketType type, uint seqno)
+			{
+				m_type = type;
+				m_seqno = seqno;
+			}
+
+			// @func Serialize
+			// @desc Serializes a PacketHeader. Assumes dst is already backed by memory. 
+			public static void Serialize(PacketHeader header, ref byte[] dst)
+			{
+				MemCpy((int)header.m_type, dst, 0);
+				MemCpy(header.m_seqno, dst, sizeof(PacketType));
+			}
+
+			public static byte[] Serialize(PacketHeader header)
+			{
+				byte[] buf = Malloc(header);
+				MemCpy((int)header.m_type, buf, 0);
+				MemCpy(header.m_seqno, buf, sizeof(PacketType));
+				return buf;
+			}
+
+			public static PacketHeader Deserialize(byte[] buf)
+			{
+				PacketType type = (PacketType)BitConverter.ToInt32(buf, 0);
+				uint seqno = BitConverter.ToUInt32(buf, sizeof(PacketType));
+				return new PacketHeader(type, seqno);
+			}
+		}
+
+		// @class ClientHistory
+		// @desc Maintains the client's history of snapshots for client-side prediction and delta compression. 
+		// TODO: ClientHistory doesn't have to have any knowledge of the game itself -- only the type that's been defined
+		// as the player snapshot -- so the snapshot should be a "template" argument. 
+		public class ClientHistory {
+			public static uint CLIENT_TIMEOUT = 5;
+			private static uint MAX_SNAPSHOTS = 10;
+
+			private PlayerSnapshot[] m_snapshots = new PlayerSnapshot[MAX_SNAPSHOTS];
+			private uint m_seqno;
+			private float m_timeSinceLastAck;
+
+			public ClientHistory(PlayerSnapshot initialPlayerState)
+			{
+				m_seqno = initialPlayerState.m_header.m_seqno;
+				m_timeSinceLastAck = 0f;
+				PutSnapshot(initialPlayerState);
+			}
+
+			// @func Reconcile
+			// @desc Reconciles the client state with the server state. If this returns false,
+			// the client player needs to be rolled back to the server's snapshot.
+			public bool Reconcile(PlayerSnapshot snapshot)
+			{
+				PlayerSnapshot predicted = GetSnapshot(snapshot.m_header.m_seqno);
+				if (predicted.m_position != snapshot.m_position) {
+					PutSnapshot(snapshot);
+					return false;
+				} else
+					return true;
+			}
+
+			public void PutSnapshot(PlayerSnapshot playerSnapshot)
+			{
+				m_timeSinceLastAck = 0f;
+				if (playerSnapshot.m_header.m_seqno > m_seqno)
+					m_seqno = playerSnapshot.m_header.m_seqno;
+				m_snapshots[playerSnapshot.m_header.m_seqno % MAX_SNAPSHOTS] = playerSnapshot;
+			}
+
+			public PlayerSnapshot GetSnapshot(uint seqno)
+			{
+				PlayerSnapshot snapshot = m_snapshots[seqno % MAX_SNAPSHOTS];
+				if (snapshot.m_header.m_seqno != seqno)
+					Debug.Log("ClientHistory: Seqnos don't match.");
+				return snapshot;
+			}
+
+			public PlayerSnapshot GetMostRecentSnapshot()
+			{
+				return m_snapshots[GetSnapshotIndex()];
+			}
+
+			public float GetTimeSinceLastAck()
+			{
+				return m_timeSinceLastAck;
+			}
+
+			public uint GetSeqno()
+			{
+				return m_seqno;
+			}
+
+			public void IncrTimeSinceLastAck(float dt)
+			{
+				m_timeSinceLastAck += dt;
+			}
+
+			// @func GetSnapshotIndex
+			// @desc Gets the index into the circular buffer of the most recent snapshot.
+			private uint GetSnapshotIndex()
+			{
+				return GetSeqno() % MAX_SNAPSHOTS;
+			}
+		}
+
+		public delegate void MainThreadWork();
+
+		public struct ClientAddress {
+			public string ipAddress;
+			public int port;
+
+			public ClientAddress(string ipAddress, int port)
+			{
+				this.ipAddress = ipAddress;
+				this.port = port;
+			}
+		}
+
+		// @doc Helpers for copying primitive types to byte arrays. 
+
+		public static void DeserializeVec3(ref Vector3 src, byte[] dst, int offset)
 		{
-			PacketHeader header = new PacketHeader();
-			header.type = (PacketType)BitConverter.ToInt32(buf, 0);
-			header.seqno = BitConverter.ToUInt32(buf, sizeof(PacketType));
-			return header;
+			src.x = BitConverter.ToSingle(dst, offset + 0 * sizeof(float));
+			src.y = BitConverter.ToSingle(dst, offset + 1 * sizeof(float));
+			src.z = BitConverter.ToSingle(dst, offset + 2 * sizeof(float));
 		}
 
-		public static byte[] Serialize(CmdPacket cmdPacket)
+		// @func Malloc
+		// @desc Allocate sizeof(obj) bytes. 
+		public static byte[] Malloc<T>(T obj)
 		{
-			byte[] buf = new byte[Marshal.SizeOf(cmdPacket)];
-			MemCpy((int)cmdPacket.header.type, buf, 0, sizeof(PacketType));
-			MemCpy(cmdPacket.header.seqno, buf, sizeof(PacketType), sizeof(uint));
-			MemCpy((int)cmdPacket.cmd, buf, Marshal.SizeOf(cmdPacket.header), sizeof(int));
-			return buf;
+			return new byte[Marshal.SizeOf(obj)];
 		}
 
-		public static CmdPacket Deserialize(byte[] buf)
+		public static void MemCpy(Vector3 src, byte[] dst, int offset)
 		{
-			PacketHeader header = GetHeader(buf);
-			CmdPacket cmdPacket = new CmdPacket();
-			cmdPacket.header = header;
-			cmdPacket.cmd = (CmdType)BitConverter.ToInt32(buf, Marshal.SizeOf(header));
-			return cmdPacket;
+			MemCpy(src.x, dst, offset + 0 * sizeof(float));
+			MemCpy(src.y, dst, offset + 1 * sizeof(float));
+			MemCpy(src.z, dst, offset + 2 * sizeof(float));
 		}
 
-		public static void MemCpy(bool src, byte[] dst, int offset, int count)
+		public static void MemCpy(bool src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(bool));
 		}
 
-		public static void MemCpy(char src, byte[] dst, int offset, int count)
+		public static void MemCpy(char src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(char));
 		}
 
-		public static void MemCpy(double src, byte[] dst, int offset, int count)
+		public static void MemCpy(double src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(double));
 		}
 
-		public static void MemCpy(float src, byte[] dst, int offset, int count)
+		public static void MemCpy(float src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(float));
 		}
 
-		public static void MemCpy(int src, byte[] dst, int offset, int count)
+		public static void MemCpy(int src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(int));
 		}
 
-		public static void MemCpy(long src, byte[] dst, int offset, int count)
+		public static void MemCpy(long src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(long));
 		}
 
-		public static void MemCpy(short src, byte[] dst, int offset, int count)
+		public static void MemCpy(short src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(short));
 		}
-		public static void MemCpy(ushort src, byte[] dst, int offset, int count)
+		public static void MemCpy(ushort src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
-		}
-
-		public static void MemCpy(ulong src, byte[] dst, int offset, int count)
-		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(ushort));
 		}
 
-		public static void MemCpy(uint src, byte[] dst, int offset, int count)
+		public static void MemCpy(ulong src, byte[] dst, int offset)
 		{
-			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, count);
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(ulong));
+		}
+
+		public static void MemCpy(uint src, byte[] dst, int offset)
+		{
+			Buffer.BlockCopy(BitConverter.GetBytes(src), 0, dst, offset, sizeof(uint));
 		}
 	}
 
-	// This is a class for executing a function every N seconds.
-	// Particularly useful for network debugging...
+	// @class PeriodicFunction
+	// @desc Executes a function every N seconds. Particularly useful for network debugging...
 	public class PeriodicFunction {
 		public delegate void DoEveryNSeconds();
 
@@ -133,3 +359,4 @@ namespace FpsNetcode {
 		}
 	}
 }
+	
---
+++ Assets/Scripts/GameServer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using FpsNetcode;
+
+namespace FpsServer {
+	// @class Game
+	// @desc Simulates the game state on the server side.
+	public class GameServer : MonoBehaviour {
+		public GameObject spawnPlayerPrefab;
+
+		// @doc The game objects on the server side are hashed by their instance IDs. 
+		private Dictionary<int, GameObject> m_objects = new Dictionary<int, GameObject>();
+
+		public GameServer() {}
+
+		// @func SpawnPlayer
+		// @desc Spawns a new player and returns its initial snapshot.
+		public Netcode.PlayerSnapshot SpawnPlayer()
+		{
+			GameObject gameObject = Instantiate(spawnPlayerPrefab);
+			Netcode.PlayerSnapshot snapshot = new Netcode.PlayerSnapshot(0, gameObject.GetInstanceID(), new Vector3(0, 1, 0));
+			Netcode.ApplySnapshot(ref gameObject, snapshot);
+			m_objects[gameObject.GetInstanceID()] = gameObject;
+			return snapshot;
+		}
+
+		// @func PutSnapshot
+		// @desc Inserts the given snapshot.
+		public GameObject PutSnapshot(Netcode.PlayerSnapshot snapshot)
+		{
+			GameObject gameObject = GetEntity(snapshot.m_serverId);
+			Netcode.ApplySnapshot(ref gameObject, snapshot);
+			return gameObject;
+		}
+
+		// @func GetEntity
+		// @desc Gets the entity from the server ID. 
+		public GameObject GetEntity(int serverId)
+		{
+			return m_objects[serverId];
+		}
+
+		// @func KillEntity
+		// @desc Removes the game object with server ID from the game. 
+		public void KillEntity(int serverId)
+		{
+			GameObject killedEntity = m_objects[serverId];
+			m_objects.Remove(serverId);
+			Destroy(killedEntity);
+		}
+
+		// @func GetSnapshot
+		// @desc Gets a snapshot from the given player object.
+		public Netcode.PlayerSnapshot GetSnapshotOfPlayer(GameObject playerObject)
+		{
+			return new Netcode.PlayerSnapshot(0, playerObject.GetInstanceID(), playerObject.GetComponent<Transform>().position);
+		}
+	}
+}
+
