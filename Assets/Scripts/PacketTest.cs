284cb37fe8a8588356a12ce7f04d2cafba3d2651
Chris <chriskbachelor@gmail.com>
2018-06-04 16:08:10 -0700

    Integrating masterserver

---
+++ Assets/Scripts/PacketTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class PacketTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] playerlist = { "abc", "def" };
+        string CurrentLobby = "heljhibiubilo";
+        string MainPlayerName = "bobwewfewewffwefe";
+        Netcode.CreateLobby packet = new Netcode.CreateLobby(CurrentLobby, MainPlayerName);
+        byte[] joinBuf = Netcode.Serializer.Serialize(packet);
+        Netcode.CreateLobby des = Netcode.Serializer.Deserialize<Netcode.CreateLobby>(joinBuf);
+        Debug.Log(des.m_type);
+        //Debug.Log(des.m_listOfPlayers[1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI/ServerName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ServerName : MonoBehaviour {
+    public FpsServer.GameServer m_server;
+    public Text serverText;
+    public void GetRegionName()
+    {
+        m_server.RegionServerName = serverText.text;
+        m_server.SendRegisterServer();
+    }
+}
